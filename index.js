14e927b00e01fe24b926d51b5e7d16b8fe312d2f
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-08 13:56:39 -0500

    created basic routes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the home page.')
+app.get('/videos', (req, res) => {
+    //Send back videos data
 });
+
+app.post('/videos', (req, res) => {
+    //Create a new video from upload form
+});
+
+app.get('/videos/:id', (req, res) => {
+    //Send back data for specific video using req.params
+});
+
+
 
 app.listen(8080, () => {
     console.log('listening on port 8080')
