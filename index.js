165e5c476b02865a33debceee3c3d51421c7912a
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-08 14:03:49 -0500

    added routes for diving deeper

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ app.get('/videos/:id', (req, res) => {
     //Send back data for specific video using req.params
 });
 
+//Diving deeper routes
+app.post('/videos/:id/comments', (req, res) => {
 
+})
+
+app.delete('/videos/:id/comments/:commendId', (req, res) => {
+
+})
 
 app.listen(8080, () => {
     console.log('listening on port 8080')
