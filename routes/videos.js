5e132e8f5011bbb1f129bcb6bff42011f8d3d2fa
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-09 01:56:14 -0500

    comments/:id route now functioning

--- routes/videos.js
+++ routes/videos.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const { route } = require('express/lib/application');
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 const getData = () => {
     const videos = fs.readFileSync('./data/videos-data.json')
     return JSON.parse(videos);
-}
-
-const getVideoData = () => {
-
 }
 
 router.route('/')
@@ -17,10 +13,10 @@ router.route('/')
         let formattedVideos = getData()[0].videos
         .map(video =>{
             return {
-                "id": video.id,
-                "title": video.title,
-                "channel": video.channel,
-                "image": video.image
+                id: video.id,
+                title: video.title,
+                channel: video.channel,
+                image: video.image
             }
         })
         res.status(200).json(formattedVideos)
@@ -32,25 +28,31 @@ router.route('/')
 
 router.route('/:id')
     .get((req, res) => {
-    //Send back data for specific video using req.params
     let formattedVideoData = getData()[1].videoData
     .map(video =>{
         return {
-            "id": video.id,
-            "title": video.title,
-            "channel": video.channel,
-            "image": video.image,
-            "description": video.description,
-            "views": video.views,
-            "likes": video.likes,
-            "duration": video.duration,
-            "video": video.video,
-            "timestamp": video.timestamp,
-            "comments": video.comments
+            id: video.id,
+            title: video.title,
+            channel: video.channel,
+            image: video.image,
+            description: video.description,
+            views: video.views,
+            likes: video.likes,
+            duration: video.duration,
+            video: video.video,
+            timestamp: video.timestamp,
+            comments: video.comments.map(comment => {
+                return {
+                    name: comment.name,
+                    comment: comment.comment,
+                    id: uuidv4(),
+                    likes: comment.likes,
+                    timestamp: comment.timestamp
+                }
+            })
         }  
     })
     res.status(200).json(formattedVideoData.find(video=>video.id === req.params.id))
-    
     });
 
 router.route('/:id/comments')
