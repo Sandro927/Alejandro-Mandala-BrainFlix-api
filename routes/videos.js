ed48259def06afb7bb1d5e579fb09610a2d148a7
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-09 01:15:46 -0500

    added logic to fetch specified video data

--- routes/videos.js
+++ routes/videos.js
@@ -3,14 +3,18 @@ const { route } = require('express/lib/application');
 const router = express.Router();
 const fs = require("fs");
 
-const getVideos = () => {
-    const videos = fs.readFileSync('./data/videos.json')
+const getData = () => {
+    const videos = fs.readFileSync('./data/videos-data.json')
     return JSON.parse(videos);
+}
+
+const getVideoData = () => {
+
 }
 
 router.route('/')
     .get((req, res) => {
-        let formattedVideos = getVideos()[0].videos
+        let formattedVideos = getData()[0].videos
         .map(video =>{
             return {
                 "id": video.id,
@@ -20,13 +24,33 @@ router.route('/')
             }
         })
         res.status(200).json(formattedVideos)
+    })
+    .post((req, res) => {
+        //create a new video
     });
     
 
 router.route('/:id')
     .get((req, res) => {
     //Send back data for specific video using req.params
-    res.send('videos/:id route');
+    let formattedVideoData = getData()[1].videoData
+    .map(video =>{
+        return {
+            "id": video.id,
+            "title": video.title,
+            "channel": video.channel,
+            "image": video.image,
+            "description": video.description,
+            "views": video.views,
+            "likes": video.likes,
+            "duration": video.duration,
+            "video": video.video,
+            "timestamp": video.timestamp,
+            "comments": video.comments
+        }  
+    })
+    res.status(200).json(formattedVideoData.find(video=>video.id === req.params.id))
+    
     });
 
 router.route('/:id/comments')
