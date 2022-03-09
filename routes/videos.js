99364188193596dff23770cf0dfd354d224c24b1
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-08 23:54:12 -0500

    created a function to read data from json file

--- routes/videos.js
+++ routes/videos.js
@@ -3,25 +3,32 @@ const { route } = require('express/lib/application');
 const router = express.Router();
 const fs = require("fs");
 
+const getVideos = () => {
+    const videos = fs.readFileSync('./data/videos.json')
+    return JSON.parse(videos);
+}
+
 router.route('/')
     .get((req, res) => {
         //get videos
+        res.status(200).json(getVideos())
     });
     
 
 router.route('/:id')
     .get((req, res) => {
     //Send back data for specific video using req.params
+    res.send('videos/:id route');
     });
 
 router.route('/:id/comments')
     .post((req, res) => {
-
+      
 });
 
 router.route('/:id/comments/:commendId')
     .delete((req, res) => {
-
+       
     });
 
 module.exports = router;
